6c2361f539d106de667bd81d1f4f189b65b582b5
Quentin D <dupertuis_quentin@hotmail.fr>
2013-11-12 09:44:07 +0100

    Initial commit

---
+++ projet2/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c7019aaf7c32254a37e99ffd8b0ff700fc97d4a52d0679fc4e286ebfd57910129c95277ce6e8e658025d63d55fe550ece50e00261065d8c39a45faad095c161e'
---
+++ rails_quentin/quentin_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuentinApp::Application.config.secret_token = '6029e41ad2213a441824801884fea150f338674227cde6140a5b01739ead42cfe62fcba7e16b50cce39ce416ca90d35903236cfbfe64cc33c7bdc93ab90497b3'
